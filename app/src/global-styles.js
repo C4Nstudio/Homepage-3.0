3177c3b95d019defb8c90dff0ce87b7cda52f097
C4Nstudio <C4Nstudio@foxmail.com>
2019-08-05 14:44:10 +0800

    refactor with create-next-app

--- app/src/global-styles.js
+++
@@ -1,8 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-export const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'lobster';
-    src: url('src/assets/fonts/Lobster-Regular.ttf') format('truetype');
-  }
-`
